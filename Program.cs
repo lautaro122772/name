ad67bd5191da17888e215f517f93cf2d7ea976b3
lol <lautaro122772>
2024-11-07 20:42:44 -0300

    mondongo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿﻿using System;
+public class TablaMultiplicar
+{
+    public static void Main(string[] args)
+    {
+        int x = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine("{0}x{1}= {2}", x, i, x * i);
+        }
+        Console.ReadKey();
+    }
+}
